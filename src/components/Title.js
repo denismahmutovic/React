e225380f2932b4d5eb2d93c68c03090014636bad
Denis Mahmutovic <deny@Deniss-MacBook-Pro.local>
2022-08-08 19:07:50 +0200

    init

---
+++ src/components/Title.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Title.css"
+
+const Title = ({name,age}) => {
+    
+  return (
+    <h3> zdravo {name},imam {age} godina</h3>
+  )
+}
+
+export default Title
+
+
+
