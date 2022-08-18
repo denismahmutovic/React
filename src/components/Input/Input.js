ce7e5e47bc7a0f1c578d716d73d03a536ca96d0e
denismahmutovic <djdeny95@gmail.com>
2022-08-18 23:17:58 +0200

    login-fb

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import "./Input.css";
 const Input = () => {
   const [formValues, setFormValues] = React.useState({
     email: "",
@@ -8,18 +8,19 @@ const Input = () => {
 
   return (
     <div
-      className="container"
+      className="container-input"
       onSubmit={(e) => {
         e.preventDefault();
         console.log(formValues);
         console.log(
-          `Email${formValues.email}, password ${formValues.password}`
+          `Email ${formValues.email}, password ${formValues.password}`
         );
       }}
     >
-      <h1>Sign in</h1>
+      {" "}
+      <h2 className="fb">facebook</h2>
       <form>
-        <div className="form-email">
+        <div className="formEmail">
           <input
             type="text"
             value={formValues.email}
@@ -28,7 +29,21 @@ const Input = () => {
             }
           ></input>
         </div>
+
+        <div className="password">
+          <input
+            type={"password"}
+            value={formValues.password}
+            onChange={(e) =>
+              setFormValues((prev) => ({ ...prev, password: e.target.value }))
+            }
+          ></input>
+        </div>
       </form>
+      <button className="btn">PRIJAVI SE </button>
+      <p className="lozinka"> Zaboravili ste lozinku</p>
+      <hr></hr>
+      <button className="btn-green">Kreiraj Novi korisnicki racun</button>
     </div>
   );
 };
