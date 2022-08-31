b01a2864f66e70bf9c4cab22596eaa7797d75389
denismahmutovic <djdeny95@gmail.com>
2022-08-31 16:26:04 +0200

    vezba

--- src/components/Vezba/Vezba.js
+++ src/components/Vezba/Vezba.js
@@ -41,6 +41,7 @@ export const Vezba = () => {
               <p>{el.name}</p>
               <p>{el.bio}</p>
               <h3>{el.discription}</h3>
+              <h3>dd</h3>
               <hr />
             </div>
           ))}
